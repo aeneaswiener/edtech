7337ccc8ccd56408bb73cb87b9542f3a10a7a066
Aeneas Wiener <aeneas.wiener@gmail.com>
2013-10-26 14:12:05 +0100

    added bootstrap template example

--- edtech.py
+++ edtech.py
@@ -6,10 +6,13 @@ import jinja2
 import datetime
 import logging
 import json
+import os
 
+
+templates_folder = os.path.join(os.path.dirname(__file__),'templates')
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    loader = jinja2.FileSystemLoader(templates_folder),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
@@ -17,14 +20,14 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.headers['Content-Type'] = 'text/html'
 
         template_values = {
             'greetings': 'hello',
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write('Hello, Gethin!')
+        self.response.write(template.render(template_values))
 
 class ClassRoom(webapp2.RequestHandler):
     def put(self):
